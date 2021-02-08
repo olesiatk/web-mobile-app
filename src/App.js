3a288e9136de5cd1ee6129c0742d620410add915
Olesia <olesia.tk@gmail.com>
2021-02-08 23:07:36 +0200

    Add html-text

--- src/App.js
+++ src/App.js
@@ -1,23 +1,62 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <section className="App__page-enter">
+        <h1 className="App__header">Ready for a great User experiance?</h1>
+        <p className="App__text-info"> Bring your media to the next level!</p>
+        <button className="App__buttom">SignUp</button>
+        <button className="App__buttom-prime">-></button>
+      </section>
+
+      <section className="App__page-signUP">
+        <h1 className="App__header">Sign Up</h1>
+        <form className="App__form">
+          <label className="App__lebel">
+            <p>Full name</p>
+            <input className="App__input" placeholder="John Doe"></input>
+          </label>
+          <label className="App__lebel">
+            <p>Email</p>
+            <input className="App__input" placeholder="example@acme.com"></input>
+          </label>
+          <label className="App__lebel">
+            <p>Password</p>
+            <input className="App__input" placeholder="********"></input>
+          </label>
+          <label className="App__input">
+            <p>Repeat password</p>
+            <input className="App__input" placeholder="********"></input>
+          </label>
+
+          <button>Sign Up</button>
+        </form>
+        <p className="App__text-question"> Already have an account?</p>
+        <button className="App__link">Sign in</button>
+      </section>
+
+      <section className="App__page-signIN">
+        <h1 className="App__header">Sign In</h1>
+        <form className="App__form">
+          <label className="App__lebel">
+            <p>Email</p>
+            <input className="App__input" placeholder="example@acme.com"></input>
+          </label>
+          <label className="App__lebel">
+            <p>Password</p>
+            <input className="App__input" placeholder="********"></input>
+            <input id="password-field" type="password" className="App__input" name="password" value="secret"/>
+          </label>
+          <button>Sign Up</button>
+        </form>
+        <p className="App__text-question"> Already have an account?</p>
+        <button className="App__link">Sign in</button>
+      </section>
+
+      <section className="App__page-main">
+        <h1 className="App__header">Hello, John Doe!</h1>
+      </section>
     </div>
   );
 }
